5dd6e6ed9d43eef07501e54c572cae0c923db8ae
gopikrishnannm <gopikrishnannm01@gmail.com>
2023-01-15 15:18:07 +0530

    day1

--- TUF/Recursion/Recursion3.java
+++ TUF/Recursion/Recursion3.java
@@ -1,7 +1,44 @@
 package Recursion;
 
+import java.util.Arrays;
+
 public class Recursion3 {
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        //int[] arr = {1, 2, 3, 4, 5};
+        //revArr2(arr, 0,arr.length-1);
+        //revArr(arr, 0);
+        //System.out.println(Arrays.toString(arr));
+        String str = "madam";
+        System.out.println(isPalindrome(str,0));
+    }
+    // Palindrome checking
+    private static boolean isPalindrome(String str,int i) {
+        if (i>=str.length()/2)
+            return true;
+        if(str.charAt(i) != str.charAt(str.length()-i-1))
+            return false;
+        return isPalindrome(str, i+1);
+    }
+
+    //Using single pointer
+    private static void revArr(int[] arr, int ptr) {
+        if (ptr>=arr.length/2)
+            return;
+        int temp = arr[ptr];
+        arr[ptr] = arr[arr.length-ptr-1];
+        arr[arr.length-ptr-1] = temp;
+        revArr(arr, ptr+1);
+    }
+
+
+    //Using 2 pointers
+    private static void revArr2(int[] arr, int l, int r) {
+        if (l>=r)
+            return ;
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+        revArr2(arr, l+1,r-1);
     }
 }
---
+++ TUF/Recursion/Recursion4.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+// Multiple recursion call
+public class Recursion4 {
+    public static void main(String[] args) {
+        System.out.println(fibanacci(3));
+    }
+
+    private static int fibanacci(int n) {
+        if (n<=1)
+            return n;
+//        int last = fibanacci(n-1);
+//        int secondLast = fibanacci(n-2);
+//        return last + secondLast;
+        return fibanacci(n-1) + fibanacci(n-2);
+    }
+}

d281818834810841d552ab64508f650be842dd33
gopikrishnannm <gopikrishnannm01@gmail.com>
2023-01-15 11:30:16 +0530

    day1

---
+++ Recursion - TUF/Recursion1.java
@@ -0,0 +1,13 @@
+//Recursion-Backtracking
+public class Recursion1 {
+    static void printer(int i,int n){
+        if(i>n){
+            return;
+        }
+        printer(i+1,n);
+        System.out.println(i); // 3 2 1
+    }
+    public static void main(String[] args) {
+        printer(1,3);
+    }
+}
---
+++ Recursion - TUF/Recursion2.java
@@ -0,0 +1,29 @@
+//Finding sum of n numbers using recursion
+public class Recursion2 {
+    // Parametarized way
+    static void sumN1(int n, int sum){
+        if (n<1) {
+            System.out.println(sum);
+            return;
+        }
+        sumN1(n-1, sum+n);
+    }
+    // Functional way
+    static int sumN2(int n){
+        if (n==1)
+            return 1;
+        return n+sumN2(n-1);
+    }
+    // factorial in functional way
+    static int fact(int n){
+        if(n==1 || n==0)
+            return 1;
+        return n*fact(n-1);
+    }
+
+    public static void main(String[] args) {
+        int initialSum = 0;
+        //sumN1(3, initialSum );
+        System.out.println(fact(4));
+    }
+}

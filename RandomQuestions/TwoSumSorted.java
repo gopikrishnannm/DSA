d338ef108c379e3c1c22f4f23a59f9c8b0866ce4
gopikrishnannm <gopikrishnannm@gmail.com>
2023-06-11 16:27:30 +0530

    made a new change

---
+++ RandomQuestions/TwoSumSorted.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class TwoSumSorted {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,5,7,10,11};
+        int target = 8;
+        int lp = 0;
+        int rp = arr.length-1;
+        while(lp<rp) {
+            if(arr[lp]+arr[rp]>target){
+                rp--;
+            } else if (arr[lp]+arr[rp]<target) {
+                lp++;
+            }
+            else {
+                System.out.println(Arrays.toString(new int[]{lp,rp}));
+                break;
+            }
+        }
+
+    }
+}

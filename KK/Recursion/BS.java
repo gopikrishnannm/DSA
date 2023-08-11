6094ec9a71bf5564c28ecd23a924a26186a0b207
gopikrishnannm <gopikrishnannm@gmail.com>
2023-08-11 22:32:29 +0530

    ll22

--- KK/Recursion/BS.java
+++ KK/Recursion/BS.java
@@ -3,10 +3,11 @@ package Recursion;
 public class BS {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
-        int target = 77;
+        int target = 6;
         int s = 0;
         int e = arr.length-1;
-        System.out.println(Bsearch(arr, target, s, e ));
+        int ans = Bsearch(arr, target, s, e );
+        System.out.println(ans);
     }
     public static int Bsearch(int[] arr, int target, int s, int e){
         if (s>e){
@@ -17,7 +18,7 @@ public class BS {
             return m;
         }
         if (target<arr[m]){
-            // since return type is int we have to mention return over here . just do it
+            // since return type is int we have to mention return over here.
             return Bsearch(arr, target, s, m-1);
         }
         return Bsearch(arr, target,m+1,e);

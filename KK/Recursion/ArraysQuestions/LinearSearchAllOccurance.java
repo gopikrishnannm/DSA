db401c02af5aa2c57e1c69ecf58f3893fa6a4a04
gopikrishnannm <gopikrishnannm@gmail.com>
2023-08-20 12:52:21 +0530

    ll28

--- KK/Recursion/ArraysQuestions/LinearSearchAllOccurance.java
+++ KK/Recursion/ArraysQuestions/LinearSearchAllOccurance.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 
 public class LinearSearchAllOccurance {
     public static void main(String[] args) {
-        linearSearchAllOccurance(new int[]{1, 10, 41, 5, 41}, 41);
+        linearSearchAllOccurance2(new int[]{1, 10, 41, 5, 41},4 );
     }
     static void linearSearchAllOccurance(int[] arr, int target){
 
         helper1(arr, target,  0);
         System.out.println(list);
+    }
+    static void linearSearchAllOccurance2(int[] arr, int target){
+
+        ArrayList<Integer> ans = helper2(arr, target,  0, new ArrayList<>());
+        System.out.println(ans);
     }
     static ArrayList<Integer> list = new ArrayList<>();
     private static void helper1(int[] arr,int target,  int indexCovered){
@@ -20,5 +25,16 @@ public class LinearSearchAllOccurance {
             list.add(indexCovered);
         }
         helper1(arr, target, indexCovered+1);
+    }
+
+    private static ArrayList helper2(int[] arr, int target, int index, ArrayList<Integer> alist){
+        if (index == arr.length){
+            return alist;
+        }
+        if (arr[index] == target){
+            alist.add(index); // different function call has different reference variable but all of
+            // them are pointing to same arrayList object
+        }
+        return helper2(arr, target, index+1, alist);
     }
 }

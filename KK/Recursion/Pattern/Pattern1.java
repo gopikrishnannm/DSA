f9de26410dcabc26101935b348e018e1e1ece88b
gopikrishnannm <gopikrishnannm@gmail.com>
2023-08-30 18:41:57 +0530

    ll31

---
+++ KK/Recursion/Pattern/Pattern1.java
@@ -0,0 +1,21 @@
+package Recursion.Pattern;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        print(5, 0);
+    }
+    static void print(int r, int c){
+        if (r == 0){
+            return;
+        }
+        if (c<r){
+            System.out.print("*");
+            print(r, c+1);
+        }
+        else{
+            System.out.println();
+            print(r-1, 0);
+        }
+
+    }
+}

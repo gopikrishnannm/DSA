185ad2d041f1d74363c7958344e01e02e346741f
gopikrishnannm <gopikrishnannm@gmail.com>
2023-10-07 19:12:36 +0530

    ll37

--- KK/Recursion/Permutations/Permutations.java
+++ KK/Recursion/Permutations/Permutations.java
@@ -6,9 +6,13 @@ public class Permutations {
     public static void main(String[] args) {
         //permu("","abc");
 
-        ArrayList<String> ans = permuList("","abc");
+//        ArrayList<String> ans = permuList("","abc");
+//
+//        System.out.println(ans);
 
-        System.out.println(ans);
+        System.out.println(permuCount2("","abc", 0));
+
+       //System.out.println("ab".substring(0,1));
     }
     static void permu(String p, String up){
         if (up.isEmpty()){
@@ -39,4 +43,33 @@ public class Permutations {
         }
         return ans;
     }
+
+    static int permuCount(String p, String up){
+        if (up.isEmpty()){
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0,i);
+            String s = p.substring(i, p.length());
+            count += permuCount(f+ch+s, up.substring(1));
+        }
+        return count;
+    }
+
+    static int permuCount2(String p, String up, int count){
+        if (up.isEmpty()){
+            return count+1;
+        }
+
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0,i);
+            String s = p.substring(i, p.length());
+            count = permuCount2(f+ch+s, up.substring(1), count);
+        }
+        return count;
+    }
+
 }
